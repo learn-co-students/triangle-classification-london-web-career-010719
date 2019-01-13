7ab325d0c736481c9c844f8f52649fbbeda30e60
kkoomin <minha.koo.9@gmail.com>
2019-01-13 16:01:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def valid?
+    @x > 0 && @y > 0 && @z >0
+  end
+
+  def inequality?
+    @x+@y > @z && @x+@z > @y && @y+@z > @x
+  end
+
+  def kind
+    if valid? && inequality?
+      if @x == @y && @x == @z
+        :equilateral
+      elsif @x == @y || @y == @z || @z == @x
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError.new
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
+
 end
+
+# binding.pry
+
+# To make a custom exception, just create a new class that inherits from StandardError.
+# ex) class CustomError < StandardError
+#     end
+#     raise CustomError.new()
